a5c5d52978acf7629d826ea3d5f07e0636f4efbf
Jeremy Kansas <ymerejsasnak@gmail.com>
2020-09-19 10:55:18 -0400

    Update initial docstring and delete dead code and/or demo junk at end.

--- crud.py
+++ crud.py
@@ -1,7 +1,9 @@
 '''
 Jeremy Kansas
-CS 340
-Final
+CS 499
+Artifact 1 of 2
+Python to MongoDB interface
+Used as part of Category 1 and 3
 '''
 
 
@@ -60,10 +62,10 @@ def update(old, new):
   except Exception as e:
     result = e
   return result
-    
-  
 
-    
+
+
+
 def delete(query):
   '''
   Delete document matching given query
@@ -75,10 +77,10 @@ def delete(query):
   except Exception as e:
     result = e
   return result
-    
-  
+
+
 # other specialized operations
-  
+
 def update_volume(ticker, new_volume):
   '''
   <FINAL 2 B> Update function that specifically matches a Ticker and updates its Volume with a value greater than 0
@@ -86,13 +88,13 @@ def update_volume(ticker, new_volume):
 
   if new_volume <= 0:
     result = "Volume must be greater than zero."
-  
+
   else:
     try:
       result = update({"Ticker": ticker}, {"Volume": new_volume})
     except Exception as e:
       result = e
-  
+
   return result
 
 
@@ -104,11 +106,11 @@ def delete_ticker(ticker):
     result = delete({"Ticker": ticker})
   except Exception as e:
     result = e
-  
-  return result
-  
 
-  
+  return result
+
+
+
 def fifty_day_count(low, high):
   '''
   <FINAL 3 A i> find and count all documents with a 50-day simple moving average within the given range (low, high)
@@ -119,8 +121,8 @@ def fifty_day_count(low, high):
   except Exception as e:
     result = e
   return result
-  
-  
+
+
 def find_by_industry(industry):
   '''
   <FINAL 3 A ii> get list of all Ticker strings in a given industry
@@ -131,8 +133,8 @@ def find_by_industry(industry):
   except Exception as e:
     result = e
   return result
-    
-    
+
+
 def outstanding_shares(sector):
   '''
   <FINAL 3 B> find total outstanding shares in a sector, grouped by industry
@@ -140,83 +142,8 @@ def outstanding_shares(sector):
   try:
     result = collection.aggregate([
       {"$match": {"Sector": sector} },
-      {"$group": {"_id": "$Industry", "Total Shares Outstanding": {"$sum" : "$Shares Outstanding"} } } 
+      {"$group": {"_id": "$Industry", "Total Shares Outstanding": {"$sum" : "$Shares Outstanding"} } }
     ])
   except Exception as e:
     result = e
   return result # not sure what is expected output here -- currently if try succeeds, result will be a pymongo cursor
-  
- 
-    
-    
-    
-if __name__ == "__main__":
-  
-  # 3Ai test
-  
-  low = 0
-  high = 0.5
-  print
-  print("Count averages in range - low {} to high {} --- total documents found: {}".format(low, high, fifty_day_count(low, high)))
-  low = 0.5
-  high = 2
-  print("Count averages in range - low {} to high {} --- total documents found: {}".format(low, high, fifty_day_count(low, high)))
-  print
-  
-  
-  # 3Aii test
-  
-  industry = "Medical Laboratories & Research"
-  print
-  print("Ticker names in {} Industry --- {}".format(industry, find_by_industry(industry)))
-  print
-  
-  
-  # 3B test
-  
-  sector = "Healthcare"
-  print
-  print("Total outstanding shares by industry in sector {} ---\n".format(sector))
-  cursor = outstanding_shares(sector)
-  for doc in cursor:
-    print(doc)
-  print
-  
-  
-  
-  
-  
-  # test from previous part of project - result shown as screenshot in final document
-  
-  '''
-  # values for testing
-  insert_test = {"key" : "value"}
-  ticker = "A"
-  volume = 123456789
-  ticker2 = "BRLI"
-
-  
-  
-  # basic insert() test
-  print
-  print("INSERT {} into {} collection in {} database\n  --- {}".format(insert_test, collection_name, db_name, insert(insert_test)))
-  print
-  print("READ {} from {} collection in {} database\n  --- {}".format(insert_test, collection_name, db_name, read(insert_test)))
-  print
-
-  # basic update_volume() test
-  print
-  print("UPDATE Ticker {} with new Volume {} in {} collection in {} database\n  --- {}".format(ticker, volume, collection_name, db_name, update_volume(ticker, volume)))
-  print
-  print("READ {} from {} collection in {} database\n  --- {}".format({"Ticker":ticker}, collection_name, db_name, read({"Ticker":ticker})))
-  print
-  
-  # basic delete_ticker test
-  print
-  print("DELETE {} from {} collection in {} database\n  --- {}".format({"Ticker":ticker2}, collection_name, db_name, delete_ticker(ticker2)))
-  print
-  print("READ {} from {} collection in {} database\n  --- {}".format({"Ticker":ticker2}, collection_name, db_name, read({"Ticker":ticker2})))
-  print
-  '''
-    
-      
