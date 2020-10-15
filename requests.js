a0d6211c3a017c84da82bf04caa6d00401c927e1
Jeremy Kansas <ymerejsasnak@gmail.com>
2020-10-15 12:40:05 -0400

    Big time cleanup and refactor on requests.js

--- requests.js
+++ requests.js
@@ -1,41 +1,84 @@
+/*
+Jeremy Kansas
+CS 499
+Category 3 Artifact
+*/
 
 window.onload = function() {
 
-    function setMessage(message) {
-        const messageLabel = document.getElementById('message');
-        messageLabel.innerHTML = '\n\n' + message;
-    }
 
-
-
-    /*
-     *  CREATE
-     */
+    // Get UI elements
     const createButton = document.getElementById('create_button');
     const createTicker = document.getElementById('create_ticker');
     const createKey = document.getElementById('create_key');
     const createValue = document.getElementById('create_value');
 
-    function createItem(ticker, data) {
-        console.log('Sending POST request');
+    const readButton = document.getElementById('read_button');
+    const readTicker = document.getElementById('read_ticker');
 
-        const XHR = new XMLHttpRequest();
+    const updateButton = document.getElementById('update_button');
+    const updateTicker = document.getElementById('update_ticker');
+    const updateKey = document.getElementById('update_key');
+    const updateValue = document.getElementById('update_value');
+
+    const deleteButton = document.getElementById('delete_button');
+    const deleteTicker = document.getElementById('delete_ticker');
+
+
+    /*
+     *  Function setMessage
+     *  Displays a string in the 'message' text label
+     *  Args: message - the string to display
+     */
+    function setMessage(message) {
+        const messageLabel = document.getElementById('message');
+        messageLabel.innerHTML = '\n\n' + message;
+    }
+
+    /*
+     *  Function XHR
+     *  Sets up and sends an XMLHttpRequest
+     *  Args:
+     *        ticker: ticker symbol as string
+     *        crudOp: crud operation as string
+     *        data: (optional) data to send
+     */
+    function XHR(ticker, crudOp, data=null) {
+        const XHR = XMLHttpRequest();
+        const METHODS = {"create":"POST", "read":"GET", "update":"PUT", "delete":"DELETE"}
+
+        crudOp = crudOp.toLowerCase();
+        let reqMethod = METHODS[crudOp];
 
         // Define what happens on successful data submission
-        XHR.addEventListener('load', function(event) {
+        XHR.addEventListener( 'load', function(event) {
             setMessage(XHR.response);
         } );
 
         // Define what happens in case of error
-        XHR.addEventListener('error', function(event) {
+        XHR.addEventListener( 'error', function(event) {
             setMessage('something went wrong');
         } );
 
-        // setup and send POST request
-        let url = 'http://localhost:8080/stocks/api/v1.0/createStock/' + ticker;
-        XHR.open("POST", url);
-        XHR.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        XHR.send(JSON.stringify(data));
+        // setup and send request
+        let url = 'http://localhost:8080/stocks/api/v1.0/' + crudOp.toLowerCase() + 'Stock/' + ticker;
+        XHR.open(reqMethod, url);
+
+        if (data) {
+            XHR.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            XHR.send(JSON.stringify(data));
+        } else {
+            XHR.send();
+        }
+    }
+
+
+    /*
+     *  CREATE
+     */
+    function createItem(ticker, data) {
+        console.log('Sending POST request');
+        XHR(ticker, 'create', data);
     }
 
     // attach the function to the button
@@ -49,32 +92,12 @@ window.onload = function() {
     } );
 
 
-
     /*
      *  READ
      */
-    const readButton = document.getElementById('read_button');
-    const readTicker = document.getElementById('read_ticker');
-
     function readItem(ticker) {
         console.log( 'Sending GET request' );
-
-        const XHR = new XMLHttpRequest();
-
-        // Define what happens on successful data submission
-        XHR.addEventListener( 'load', function(event) {
-            setMessage(XHR.response);
-        } );
-
-        // Define what happens in case of error
-        XHR.addEventListener( 'error', function(event) {
-            setMessage('something went wrong');
-        } );
-
-        // setup and send request
-        let url = 'http://localhost:8080/stocks/api/v1.0/getStock/' + ticker;
-        XHR.open('GET', url);
-        XHR.send();
+        XHR(ticker, 'read');
     }
 
     // attach the function to the button
@@ -84,35 +107,12 @@ window.onload = function() {
     } );
 
 
-
     /*
      *  UPDATE
      */
-    const updateButton = document.getElementById('update_button');
-    const updateTicker = document.getElementById('update_ticker');
-    const updateKey = document.getElementById('update_key');
-    const updateValue = document.getElementById('update_value');
-
     function updateItem(ticker, data) {
         console.log('Sending PUT request');
-
-        const XHR = new XMLHttpRequest();
-
-        // Define what happens on successful data submission
-        XHR.addEventListener('load', function(event) {
-            setMessage(XHR.response);
-        } );
-
-        // Define what happens in case of error
-        XHR.addEventListener('error', function(event) {
-            setMessage('something went wrong');
-        } );
-
-        // setup and send PUT request
-        let url = 'http://localhost:8080/stocks/api/v1.0/updateStock/' + ticker;
-        XHR.open("PUT", url);
-        XHR.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        XHR.send(JSON.stringify(data));
+        XHR(ticker, 'update', data);
     }
 
     // attach the function to the button
@@ -126,32 +126,12 @@ window.onload = function() {
     } );
 
 
-
     /*
      *  DELETE
      */
-    const deleteButton = document.getElementById('delete_button');
-    const deleteTicker = document.getElementById('delete_ticker');
-
     function deleteItem(ticker) {
         console.log('Sending DELETE request');
-
-        const XHR = new XMLHttpRequest();
-
-        // Define what happens on successful data submission
-        XHR.addEventListener( 'load', function(event) {
-            setMessage(XHR.response);
-        } );
-
-        // Define what happens in case of error
-        XHR.addEventListener( 'error', function(event) {
-            setMessage('something went wrong');
-        } );
-
-        // setup and send request
-        let url = 'http://localhost:8080/stocks/api/v1.0/deleteStock/' + ticker;
-        XHR.open('DELETE', url);
-        XHR.send();
+        XHR(ticker, 'delete');
     }
 
     // attach the function to the button
@@ -159,11 +139,6 @@ window.onload = function() {
         let ticker = deleteTicker.value;
         deleteItem(ticker);
     } );
-
-
-
-
-
 
 
 }
